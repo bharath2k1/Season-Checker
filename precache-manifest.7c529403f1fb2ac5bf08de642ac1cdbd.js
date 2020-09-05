2f20b7e41e167a7673136a2df4e47a4c9e08dc37
BHARATH SIMHA REDDY <bharathreddy8345@gmail.com>
2020-09-05 08:33:17 +0530

    Updates

---
+++ precache-manifest.7c529403f1fb2ac5bf08de642ac1cdbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e7a249416e03d052a45f50e58fdbfee",
+    "url": "/Season-Checker/index.html"
+  },
+  {
+    "revision": "34db4df2f75fd12b240b",
+    "url": "/Season-Checker/static/css/main.3967cb32.chunk.css"
+  },
+  {
+    "revision": "c2d246ecc2b83ccac6a6",
+    "url": "/Season-Checker/static/js/2.8b77c9df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Season-Checker/static/js/2.8b77c9df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34db4df2f75fd12b240b",
+    "url": "/Season-Checker/static/js/main.f781591a.chunk.js"
+  },
+  {
+    "revision": "a34300ae320aadcc5fc5",
+    "url": "/Season-Checker/static/js/runtime-main.292fc200.js"
+  }
+]);
